f2975edd4b768c44f0ded7b389fc9699752d4f06
aqdodovirshod <aqdodovirshod7@gmail.com>
2025-11-28 09:07:33 +0000

    Fix nginx/static, update settings and urls

--- server/urls.py
+++ server/urls.py
@@ -5,6 +5,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 
 schema_view = get_schema_view(
